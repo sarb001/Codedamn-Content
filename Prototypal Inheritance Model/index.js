56db4e1e39be4ea7007c581cd192b985b6454511
SARB <mrsinghbusiness05@gmail.com>
2022-10-06 11:25:14 +0530

    setPrototypeo

--- Prototypal Inheritance Model/index.js
+++ Prototypal Inheritance Model/index.js
@@ -3,27 +3,36 @@
 // obj is an object whose prototype is to be returned 
 // If no inherited properties are there thn, this method will return null
 
-const a = {
-   prop1 : 1
-}
+        // const a = {
+        //    prop1 : 1
+        // }
 
-const b = {
-    prop2: 10
-}
+        // const b = {
+        //     prop2: 10
+        // }
 
-const c = {
+        // const c = {
 
-    prop3 : 1000
-}
+        //     prop3 : 1000
+        // }
 
-const d = {
-    prop4 : 10000
-}
+        // const d = {
+        //     prop4 : 10000
+        // }
 
 
 // It is kind of chaining which is used to  link Objects 
 
+// Now with the help of __proto__ we will able to link Objects and can get access to one another 
 
-d.__proto__ = c 
-c.__proto__ = b
-b.__proto__ = a 
+
+    // d.__proto__ = c 
+    // c.__proto__ = b
+    // b.__proto__ = a 
+
+
+
+    // ******
+    // Object.setPrototypeof()  ===>    This is used to  create 1 Object inside Another Object and can access it easilly 
+    // We are able to set it  Now By  itself 
+
